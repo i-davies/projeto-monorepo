2f49e6ce37a6061c14bfefea818fe6aa9b8d3fdc
Icaro Davies <idavies@outlook.com>
2026-08-17 22:26:35 -0300

    feat: sequelize e migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { sequelize } from './config/database';
+import { User } from './models/User';
 
 dotenv.config();
 
@@ -19,6 +20,38 @@ app.get('/api/health', (req: Request, res: Response) => {
         mensagem: 'Servidor Backend rodando com sucesso.',
         timestamp: new Date().toISOString()
     });
+});
+
+// Cadastrar novo usuario
+app.post('/api/users', async (req: Request, res: Response) => {
+    try{
+
+        const { nome, email, senha_hash } = req.body;
+        
+        if (!nome || !email || !senha_hash) {
+            return res.status(400).json({ erro: 'nome, email e senha_hash são obrigatórios.'})
+        }
+
+        const novoUsuario = await User.create({nome, email, senha_hash});
+
+        return res.status(201).json(novoUsuario);
+
+    } catch (error: any) {
+        return res.status(500).json({erro: 'Erro ao cadastrar usuario.', detalhe: error.message})
+    }
+});
+
+// Listar todos os usuários
+app.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const usuarios = await User.findAll({
+            attributes: ['id', 'nome', 'email', 'createdAt']
+        });
+        return res.status(200).json(usuarios);
+    }
+    catch (error: any) {
+        return res.status(500).json({erro: 'Erro ao listar usuarios.', detalhe: error.message})
+    }
 });
 
 async function main() {
